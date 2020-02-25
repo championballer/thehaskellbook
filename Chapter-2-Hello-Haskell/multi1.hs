079e1e52326755f025f9e59ff90a7d0bafc093a4
Shiv Kumar <championballer08@gmail.com>
2020-02-26 00:24:20 +0530

    Added multiplication based function based on the use of where

---
+++ Chapter-2-Hello-Haskell/multi1.hs
@@ -0,0 +1,16 @@
+-- Function definition for multi1
+
+module Mult1 where
+
+mult1    = x * y
+ where x = 5
+       y = 6
+
+mult2    = x * 3 + y
+ where x = 3
+       y = 1000
+
+mult3 = z / x + y
+ where x = 7
+       y = negate x
+       z = y * 10
