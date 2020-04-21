9b58c13a564078deead792c0e077e9a0819d1ba1
Shiv Kumar <championballer08@gmail.com>
2020-04-21 07:19:05 +0530

    Added code for chapter 22

---
+++ Chapter-22-Reader/tupled.hs
@@ -0,0 +1,31 @@
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = cap . rev
+
+fmapped :: [Char] -> [Char]
+fmapped = cap <$> rev
+
+tupled :: [Char] -> ([Char],[Char])
+tupled = (,) <$> cap <*> rev
+
+tupled' :: [Char] -> ([Char],[Char])
+tupled' = do
+    a <- cap
+    b <- rev
+    return (a,b)
+
+tupled'' :: [Char] -> ([Char],[Char])
+tupled'' = composed >>= (,) 
+
+tupled''' :: [Char] -> ([Char],[Char])
+tupled''' = 
+    cap >>=
+        \x -> rev >>=
+            \y -> return (x,y)
